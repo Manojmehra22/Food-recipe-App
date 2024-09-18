3b4710277248594606e77759c14045f337c9b1c1
Manojmehra22 <manojmehr650@gmail.com>
2024-09-18 23:07:37 +0530

    food recipe added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Mainpage from "./Component/Mainpage";
+import MealInfo from "./Component/MealInfo";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Mainpage />} />
+      <Route path="/:idMeal" element={<MealInfo />} />
+    </Routes>
+  );
+};
+
+export default App;
---
+++ src/Component/MealInfo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MealInfo = () => {
+  return <div>MealInfo</div>;
+};
+
+export default MealInfo;
